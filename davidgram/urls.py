5fecf66fc1a39db270f15deb3d332d61e540c299
David <davidsantander9@gmail.com>
2021-08-31 15:11:03 -0500

    hi view

--- davidgram/urls.py
+++ davidgram/urls.py
@@ -7,5 +7,6 @@ from davidgram import views
 
 urlpatterns = [
     path('hello-world/', views.hello_world),
-    path('sorted_numbers/', views.sorted_numbers)
+    path('sorted_numbers/', views.sorted_numbers),
+    path('hi/<str:name>/<int:age>/', views.say_hi)
 ]
--- davidgram/views.py
+++ davidgram/views.py
@@ -15,6 +15,15 @@ def hello_world(request):
     )
 
 def sorted_numbers(request):
+    """Return a JSON response with sorted numbers"""
     numbers = request.GET['numbers'].split(",")
     numbers.sort()
     return JsonResponse({'status':'ok','numbers':numbers})
+
+def say_hi(request, name, age):
+    """Return a greeting"""
+    if age < 12:
+        message = 'Sorry {}, you are no allowed here'.format(name)
+    else:
+        message = 'Hi, {}! Welcome to Davidgram'.format(name)
+    return HttpResponse(message)
