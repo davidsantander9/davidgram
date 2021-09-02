138c9ec38b0268c43dd8fd7cc1da4e3c03f0fe42
David <davidsantander9@gmail.com>
2021-09-01 20:33:49 -0500

    delete models and database

--- davidgram/urls.py
+++ davidgram/urls.py
@@ -1,4 +1,6 @@
 
+# Django
+from django.contrib import admin 
 from django.urls import path
 
 from davidgram import views as local_views
@@ -6,6 +8,9 @@ from davidgram import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
     path('hello-world/', local_views.hello_world),
     path('sorted_numbers/', local_views.sorted_numbers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
